bfe32dfadd912cfc5cebf13bf67fc3bf621f42a0
rivu <58026024+unphydra@users.noreply.github.com>
2020-01-13 09:52:38 +0530

    creating grid is extracted to another file

---
+++ grid.js
@@ -0,0 +1,26 @@
+const NUM_OF_COLS = 100;
+const NUM_OF_ROWS = 60;
+
+const GRID_ID = "grid";
+
+const getGrid = () => document.getElementById(GRID_ID);
+const getCellId = (colId, rowId) => colId + "_" + rowId;
+
+const getCell = (colId, rowId) =>
+  document.getElementById(getCellId(colId, rowId));
+
+const createCell = function(grid, colId, rowId) {
+  const cell = document.createElement("div");
+  cell.className = "cell";
+  cell.id = getCellId(colId, rowId);
+  grid.appendChild(cell);
+};
+
+const createGrids = function() {
+  const grid = getGrid();
+  for (let y = 0; y < NUM_OF_ROWS; y++) {
+    for (let x = 0; x < NUM_OF_COLS; x++) {
+      createCell(grid, x, y);
+    }
+  }
+};
